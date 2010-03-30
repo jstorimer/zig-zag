7a310b67ca929e42c9a347770574e0fbf3f2a4f4
Jesse Storimer <jstorimer@gmail.com>
2010-03-29 22:31:01 -0400

    Remove some comments; Working on controls

--- game_objects/player.rb
+++ game_objects/player.rb
@@ -2,7 +2,7 @@ class Player < Chingu::GameObject
   has_traits :collision_detection, :effect, :velocity
   has_trait :bounding_box
   
-  attr_accessor :rising, :accel_rate
+  attr_accessor :colliding, :accel_rate
   
   FALLING_RATE = 1.05
   RISING_RATE = 0.95
@@ -14,37 +14,23 @@ class Player < Chingu::GameObject
     @image = Gosu::Image["blimp.png"]
     self.input = {:holding_up=>:rise}
     self.max_velocity = 5
-    # self.accel_rate = 0.1
-    
-    # self.rising = false
+
     self.acceleration_y = 0.1
   end
   
   def rise
-    # return if on_top_edge?
-    # self.rising = true
-    
-    # if accel_rate < MAX_ACCEL_RATE
-      self.accel_rate *= 1.02
-    # end
+    self.accel_rate *= 1.02
     
     self.acceleration_y = Gosu::offset_y(self.angle, self.accel_rate)*self.max_velocity
   end
   
   def update
-    # if on_top_edge?
-    #   self.acceleration_y = FALLING_RATE
-    #   self.falling = true
-    #   return 
-    # elsif on_bottom_edge?
-    #   self.velocity_y = 0
-    #   return
-    # end
-
-    # if velocity_y < 0 && velocity_y > -2
-    #   self.falling = true
-    # end
-    # puts acceleration_y
+    puts @x
+    # puts colliding?
+    if colliding?
+      self.velocity_y = 0
+      return 
+    end
     
     if !$window.button_down?(Gosu::KbUp)
       self.accel_rate = 0.1
@@ -53,18 +39,8 @@ class Player < Chingu::GameObject
       else
         self.acceleration_y *= FALLING_RATE
       end
-    # else
-    #   # decrement acceleration
-    #   self.velocity_y *= RISING_RATE
     end
   end
   
-  private
-  def on_top_edge?
-    @y <= 0 + bounding_box.height
-  end
-  
-  def on_bottom_edge?
-    @y >= $window.height - bounding_box.height
-  end
+  def colliding?; colliding; end
 end
--- game_states/level1.rb
+++ game_states/level1.rb
@@ -5,7 +5,6 @@ end
 class Level1 < Chingu::GameState
   FILE = "media/level1.txt"
   
-  # attr_accessor :screenx
   ROCK_PADDING = 600
 
   def initialize
@@ -14,28 +13,16 @@ class Level1 < Chingu::GameState
      @tileset = Gosu::Image.load_tiles($window, "media/CptnRuby Tileset.png", 60, 60, true)
 
      self.input = { [:q, :escape] => :exit }
-     # self.screenx = 0
      
      lines = File.readlines(FILE).map { |line| line.chomp }
      @height = lines.size
      @width = lines[0].size
      @width.times do |x|
        @height.times do |y|
-     # @tiles = Array.new(@width) do |x|
-     #   Array.new(@height) do |y|
          case lines[y][x, 1]
          when '#'
-           # x = 7
-           # 7 * 50 - 5 = 345
-           
-           # y = 4
-           # 4 * 50 - 0 - 600 * 50
-           rock = Rock.create(:x => x * 60 + ROCK_PADDING, :y => (y/@height.to_f) * $window.height)
+           rock = Rock.create(:x => x * 56 + ROCK_PADDING, :y => (y/@height.to_f) * $window.height)
            # rock.hide!
-           
-           # Tiles::Rock
-         else
-           nil
          end
        end
      end
@@ -45,18 +32,6 @@ class Level1 < Chingu::GameState
    def draw
      Gosu::Image["Space.png"].draw(0, 0, 0)
 
-     # Very primitive drawing function:
-     # Draws all the tiles, some off-screen, some on-screen.
-     # @height.times do |y|
-     #   @width.times do |x|
-     #     tile = @tiles[x][y]
-     #     if tile
-     #       # rock = Rock.create(:x => x * 50 + 100, :y => (y/@height.to_f) * $window.height)
-     #       @tileset[tile].draw(x * 50 + screenx + ROCK_PADDING, (y/@height.to_f) * $window.height, 10)
-     #     end
-     #   end
-     # end
-     
      super
    end
    
@@ -67,10 +42,13 @@ class Level1 < Chingu::GameState
        rock.x -= Config::SCROLL_SPEED
        # rock.show! if rock.visible?
      end
-
+     
+     @player.colliding = false
      Player.each_collision(Rock) do |player, rock|
-       @player.x -= Config::SCROLL_SPEED
-       @player.y
+       puts 'changin x'
+       # @player.x -= Config::SCROLL_SPEED
+       @player.colliding = true
+       # @player.y
        break
      end
 
